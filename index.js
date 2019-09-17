e238bdedd422410775ae968aaf782bf52685de43
AdamSiekierski <adam.siekiera@outlook.com>
2019-09-17 21:24:40 +0200

    Fix CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 const router = require("./router")
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use("/", router)
 
