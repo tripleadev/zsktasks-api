e8854c36f639b3f62cf9819605529bbe1a8acb9a
datejer <ejer.priv@wp.pl>
2019-10-04 20:47:24 +0200

    Add Timetable

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const { check, validationResult } = require('express-validator')
 const chance = require('chance')()
 const moment = require('moment')
+const fs = require('fs')
 const { Task } = require('../models')
+let timetable = JSON.parse(fs.readFile('../timetable.json'))
 
 router.get('/', (req, res) => {
   return res.json({
-    message: 'welcome to zskTasks-api'
+    message: 'welcome to zskTasks-api',
   })
 })
 
@@ -16,17 +18,17 @@ router.post(
     check('title', 'Podaj tytuł zadania').isLength({ min: 4, max: 30 }),
     check('description', 'Podaj opis zadania').isLength({ min: 4, max: 500 }),
     check('date', 'Podaj datę wykonania zadania').isISO8601(),
-    check(
-      'subject',
-      'Podaj przedmiot, na który zostało zadane zadanie'
-    ).isLength({ min: 2, max: 30 }),
-    check('uploadCode').custom(code => {
+    check('subject', 'Podaj przedmiot, na który zostało zadane zadanie').isLength({
+      min: 2,
+      max: 30,
+    }),
+    check('uploadCode').custom((code) => {
       if (code === process.env.UPLOAD_CODE) {
         return true
       } else {
         throw new Error('Zły kod zabezpieczający')
       }
-    })
+    }),
   ],
   async (req, res) => {
     const errors = validationResult(req)
@@ -34,7 +36,7 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({
         message: 'Errors in request',
-        errors: errors
+        errors: errors,
       })
     }
 
@@ -49,7 +51,7 @@ router.post(
       title: req.body.title,
       date: moment(req.body.date).format('YYYY-MM-DD'),
       description: req.body.description,
-      subject: req.body.subject
+      subject: req.body.subject,
     })
 
     newTask
@@ -61,30 +63,30 @@ router.post(
             title: req.body.title,
             date: moment(req.body.date).format('YYYY-MM-DD'),
             description: req.body.description,
-            subject: req.body.subject
-          }
+            subject: req.body.subject,
+          },
         })
       })
-      .catch(err => console.log(err));
-  }
+      .catch((err) => console.log(err))
+  },
 )
 
 router.get('/all', (req, res) => {
   Task.forge()
     .orderBy('date')
     .fetchAll()
-    .then(tasks => {
-      const filtered = tasks.filter(task => {
+    .then((tasks) => {
+      const filtered = tasks.filter((task) => {
         const date = moment(task.attributes.date)
 
         return date.isAfter(moment().subtract(1, 'days')) ? task : null
       })
 
       const responseObject = {
-        tasks: []
+        tasks: [],
       }
 
-      filtered.map(task => {
+      filtered.map((task) => {
         const d = moment(task.attributes.date)
 
         const correctedTask = {
@@ -92,7 +94,7 @@ router.get('/all', (req, res) => {
           description: task.attributes.description,
           subject: task.attributes.subject,
           date: d.format('DD/MM/YYYY'),
-          id: task.attributes.task_id
+          id: task.attributes.task_id,
         }
 
         responseObject.tasks.push(correctedTask)
@@ -100,6 +102,10 @@ router.get('/all', (req, res) => {
 
       res.json(responseObject)
     })
+})
+
+router.get('/timetable', (req, res) => {
+  res.json(timetable)
 })
 
 module.exports = router
