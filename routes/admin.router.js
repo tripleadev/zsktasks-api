d038980f5f39ea31dc12f18415564b6ab0815911
AdamSiekierski <adam.siekiera@outlook.com>
2019-10-11 21:33:12 +0200

    Change delete task method

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -26,37 +26,41 @@ router.post('/login', (req, res, next) => {
   })(req, res, next)
 })
 
-router.post('/register', [
-  check('name').isLength({ min: 3, max: 80 }),
-  check('email').isEmail(),
-  check('password').exists(),
-  check('adminCode').exists()
-], (req, res, next) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Errors in request',
-      errors
-    })
-  }
-  passport.authenticate('register', { session: false }, (err, user, info) => {
-    if (err || !user) {
-      return res.status(400).json({
-        message: 'Something is wrong',
-        info: info,
+router.post(
+  '/register',
+  [
+    check('name').isLength({ min: 3, max: 80 }),
+    check('email').isEmail(),
+    check('password').exists(),
+    check('adminCode').exists(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: 'Errors in request',
+        errors,
       })
     }
-    req.login(user, { session: false }, () => {
-      const loginToken = jwt.sign(
-        {
-          userId: user.UserID,
-        },
-        process.env.JWT_SECRET,
-      )
-      return res.json({ message: 'Successfully created new user', loginToken, user })
-    })
-  })(req, res, next)
-})
+    passport.authenticate('register', { session: false }, (err, user, info) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: 'Something is wrong',
+          info: info,
+        })
+      }
+      req.login(user, { session: false }, () => {
+        const loginToken = jwt.sign(
+          {
+            userId: user.UserID,
+          },
+          process.env.JWT_SECRET,
+        )
+        return res.json({ message: 'Successfully created new user', loginToken, user })
+      })
+    })(req, res, next)
+  },
+)
 
 router.get('/user', passport.authorize('jwt', {}), (req, res) => {
   return new User({ UserID: req.account.userId }).fetch().then((user) => {
@@ -70,55 +74,72 @@ router.get('/user', passport.authorize('jwt', {}), (req, res) => {
   })
 })
 
-router.delete('/delete_task', passport.authorize('jwt', {}), [
-  check('task_id', 'Podaj identyfikator zadania do usunięcia').isLength({ min: 8, max: 8 })
-], (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Errors in request',
-      errors
-    })
-  }
-  return new Task()
-    .where({ task_id: req.body.task_id })
-    .destroy()
-    .then(() => {
-      res.json({
-        message: `Successfully deleted the task with id: ${req.body.task_id}`,
+router.post(
+  '/delete_task',
+  passport.authorize('jwt', {}),
+  [check('task_id', 'Podaj identyfikator zadania do usunięcia').isLength({ min: 8, max: 8 })],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: 'Errors in request',
+        errors,
       })
-    })
-})
-
-router.post('/edit_task', passport.authorize('jwt', {}), [
-  check('task_id', 'Podaj identyfikator zadania do usunięcia').isLength({ min: 8, max: 8 })
-], (req, res) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Errors in request',
-      errors: errors,
-    })
-  }
-  console.log(req.body.task_id)
-  Task.where('task_id', req.body.task_id).fetch().then((task) => {
-    if (validator.isLength(req.body.title, { min: 4, max: 30 })) { task.set('title', req.body.title) }
-    if (validator.isLength(req.body.description, { min: 4, max: 500 })) { task.set('description', req.body.description) }
-    if (validator.isISO8601(req.body.date)) { task.set('date', req.body.date) }
-    if (validator.isLength(req.body.subject, { min: 2, max: 30 })) { task.set('subject', req.body.subject) }
-
-    task.save(null, { method: 'update', patch: 'true' }).then(task => {
-      res.json({
-        message: 'Task corrected',
-        task: task.attributes
+    }
+    return new Task()
+      .where({ task_id: req.body.task_id })
+      .destroy()
+      .then(() => {
+        res.json({
+          message: `Successfully deleted the task with id: ${req.body.task_id}`,
+        })
       })
-    })
-  }).catch(err => {
-    console.log(err)
-    res.status(400).json('Task not found')
-  })
-})
+  },
+)
+
+router.post(
+  '/edit_task',
+  passport.authorize('jwt', {}),
+  [check('task_id', 'Podaj identyfikator zadania do usunięcia').isLength({ min: 8, max: 8 })],
+  (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: 'Errors in request',
+        errors: errors,
+      })
+    }
+    console.log(req.body.task_id)
+    Task.where('task_id', req.body.task_id)
+      .fetch()
+      .then((task) => {
+        if (validator.isLength(req.body.title, { min: 4, max: 30 })) {
+          task.set('title', req.body.title)
+        }
+        if (validator.isLength(req.body.description, { min: 4, max: 500 })) {
+          task.set('description', req.body.description)
+        }
+        if (validator.isISO8601(req.body.date)) {
+          task.set('date', req.body.date)
+        }
+        if (validator.isLength(req.body.subject, { min: 2, max: 30 })) {
+          task.set('subject', req.body.subject)
+        }
+
+        task.save(null, { method: 'update', patch: 'true' }).then((task) => {
+          res.json({
+            message: 'Task corrected',
+            task: task.attributes,
+          })
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(400).json('Task not found')
+      })
+  },
+)
 
 router.get('/all', passport.authorize('jwt', {}), (req, res) => {
   Task.forge()
@@ -135,7 +156,7 @@ router.get('/all', passport.authorize('jwt', {}), (req, res) => {
           description: task.attributes.description,
           subject: task.attributes.subject,
           date: d.format('DD/MM/YYYY'),
-          id: task.attributes.task_id
+          id: task.attributes.task_id,
         }
 
         correctedTasks.push(correctedTask)
